5f093ddba539a47b99ace3088b3895de211474a8
Anna <test@mail.ru>
2022-07-19 16:45:34 +0300

    finish the first slide

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 200,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clicable:true
   },
-  mousewheel: true,
-  keyboard: true,
+  autoplay:{
+    delay:1500,
+    disableOnIteraction:false,
+  }
+  
 });
+
+
+
+
+
---
+++ js/slider_courses.js
@@ -0,0 +1,17 @@
+const swiperS = new Swiper(".mySwiperS", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clicable:true
+    },
+  
+  });
+  
+  
+  
+  
+  
