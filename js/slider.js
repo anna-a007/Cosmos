70cc3b5cb26d007c9a5266b25e54fba2bc245d1e
Anna <test@mail.ru>
2022-07-22 14:48:30 +0300

    create slider2

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,36 @@
 const swiper = new Swiper(".swiper-container.swiper-one", {
-  spaceBetween: 350,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   speed: 800,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination1",
     clicable:true
   },
-  autoplay:{
-    delay:2500,
-    disableOnIteraction:false,
+  // autoplay:{
+  //   delay:2500,
+  //   disableOnIteraction:false,
   
-  }
+  // }
 
   
 });
 
-
-
+var swiperTwo = new Swiper('.swiper-container2.swiper-two', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: '.modal-swiper-button-next',
+    prevEl: '.modal-swiper-button-prev',
+  },
+  pagination: {
+    el: ".modal-swiper-pagination",
+    clicable:true
+  },
+});
 
 
